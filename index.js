8de7a02899795f1103633233e4dc5d97a99aee96
ptw3016 <132131274+ptw3016@users.noreply.github.com>
2023-09-12 20:37:44 +0900

    sdbgdlrun 12

--- index.js
+++ index.js
@@ -2,48 +2,48 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sddrctpr = require("./sddrctpr");
 const app = express();
-
 const port = 3000;
 require("dotenv").config();
+
+
+const dynamicRoute = '/';
+const AMPNM = process.env.AMPNM;
 
 app.use(express.static('node_modules/bootstrap/dist'));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get(dynamicRoute, (req, res) => {
   const path = require('path');
   const htmlFilePath = path.join(__dirname, 'sdbgdlprcs.html');
   res.sendFile(htmlFilePath);
 });
 
-app.post('/submit', (req, res) => {
+app.post('/submit', async (req, res) => {
   const { body } = req;
   //console.log(body);
-  if (body.enteredCode == "123456") {
-    res.json({ message: '비밀번호 일치!.' });
-    try {
-      var sendemjson = {
-        to: process.env.sdadminnvml,
-        subject: "제목",
-        message: "내용"
-      }
-      sddrctpr.smtbgdldvcPr("open");
-      sddrctpr.sendemailPr(sendemjson);
+  const pcchkinput = body.enteredCode;
+  const pschkrst = await sddrctpr.chkpasscd("시간", pcchkinput); //큐알로 방금 접속했는지도 확인해보기.. 위치는 어떻게..
+  // console.log(pschkrst.chkrst);
+  // console.log(pschkrst.chkrstnm);
 
-    } catch (e) {
-      var sendemjson = {
-        to: process.env.sdadminnvml,
-        subject: "제목",
-        message: "----errormsg----\n" +
-          e.message + "\n" +
-          "----errorstack----\n" +
-          e.stack + "\n"
-      }
-   
-      sddrctpr.sendemailPr(sendemjson);
+  if (pschkrst.chkrst == true) {
+    var rstmsg = "";
+    var admsg = ""; 
+    const drctrst = await sddrctpr.smtbgdldvcPr("open");
+    if (drctrst.result == "0001") {
+      rstmsg = "이미 도어락이 열려있습니다."
+    }else if(drctrst.result == "0002"){
+      rstmsg = "도어락에 문제가 있습니다.";
+      admsg = `[관리자_문의-${AMPNM}]`;
+    }else if(drctrst.result == "0000"){
+      rstmsg = pschkrst.chkrstnm + "님 도어락열기 실행성공!!.";
     }
+    
+    res.json({ message: rstmsg, admsg: admsg});
 
   } else {
-    res.json({ message: '비밀번호 불일치!.' });
+    res.json({ message: `비밀번호가 틀렸습니다. `, admsg:`[관리자_문의-${AMPNM}]` });
+    //로그 준비
   }
 });
 
--- sddrctpr.js
+++ sddrctpr.js
@@ -1,4 +1,5 @@
 const { SmartThingsClient, BearerTokenAuthenticator } = require('@smartthings/core-sdk')
+const { google } = require('googleapis');
 const { OAuth2Client } = require('google-auth-library');
 const nodemailer = require("nodemailer");
 
@@ -12,9 +13,11 @@ const sdbgdrfbon_dvId = process.env.sdbgdrfbon_dvId; //별관도어락fbt ON
 const sdbgdrfboff_dvId = process.env.sdbgdrfboff_dvId; //별관도어락fbt OFF
 
 const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET= process.env.CLIENT_SECRET;
-const REDIRECT_URI= process.env.REDIRECT_URI;
-const REFRESH_TOKEN= process.env.REFRESH_TOKEN;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const REFRESH_TOKEN2 = process.env.REFRESH_TOKEN2;
+const SHEET_ID = process.env.SHEET_ID;
 
 function sleep(ms) {
     return new Promise(resolve => {
@@ -23,8 +26,8 @@ function sleep(ms) {
 }
 
 async function smtbgdldvcPr(drctval) {     //
-    if(drctval=="open" || drctval=="closed"){
-    }else{
+    if (drctval == "open" || drctval == "closed") {
+    } else {
         console.log("제어 value가 유효하지않음 종료!")
         return;
     }
@@ -35,14 +38,15 @@ async function smtbgdldvcPr(drctval) {     //
     try {
         const rtgetstatus = await client.devices.getStatus(sdbgdrsr_dvId);  //현재상태 확인 위해 한번검사!
         const rtgetresult = rtgetstatus.components.main.contactSensor.contact.value;
-        console.log("현재상태확인:"+rtgetresult);
+        console.log("현재상태확인:" + rtgetresult);
 
-        return;
-        if(rtgetresult==doorctval){
-            console.log("현재상태가 '"+doorctval+"' 이므로 실행없이 종료!");
-            return;
+        //return;
+        if (rtgetresult == doorctval) {
+            console.log("현재상태가 '" + doorctval + "' 이므로 실행없이 종료!");
+            return { result: "0001" };
         }
-
+        return { result: "0000" };
+        //return;
         var totalct = 6  //fbt 최대 실행 횟수
         for (let i = 1; i <= totalct; i++) {
             var fbrunrst = await sdbgfbonoffPr();
@@ -53,20 +57,29 @@ async function smtbgdldvcPr(drctval) {     //
                 console.log(`-장치 요청결과 : '${result}' 확인.`);
                 if (result === doorctval) {
                     console.log(i + "/" + totalct + "번만에 원하는 값 " + doorctval + " 나왔음! 종료합니다.");
-                    return;
-                }else{
-                    console.log(i+"/"+totalct+"번째 실행 후 상태 확인실패 다시시도!");
+                    return { result: "0000" };
+                } else {
+                    console.log(i + "/" + totalct + "번째 실행 후 상태 확인실패 다시시도!");
                 }
-            }else{
-                console.log(i+"번째 요청실패! 다시시도!");
+            } else {
+                console.log(i + "번째 요청실패! 다시시도!");
             }
             if (i === totalct) {
                 console.log(`문열기를 못했습니다. 원하는 상태(${doorctval})를 받아오지 못했습니다. 최대 시도 횟수 초과`);
-                return;
+                return { result: "0002" };
             }
         }
     } catch (error) {
         console.error(error.message);
+        var sendemjson = {
+            to: process.env.sdadminnvml,
+            subject: "sdbgdl 실행중 error",
+            message: "----errormsg----\n" +
+                error.message + "\n" +
+                "----errorstack----\n" +
+                error.stack + "\n"
+        }
+        sendemailPr(sendemjson);
     }
 }
 
@@ -76,14 +89,14 @@ async function retryUntilValue_dltb(dvid, maxAttempts, targetValue) {
         const result = getstatus.components.main.contactSensor.contact.value;
 
         if (result === targetValue) {
-            return result; 
+            return result;
         }
 
         if (attempt === maxAttempts) {
             return "";
         }
 
-        await sleep(100); 
+        await sleep(100);
     }
 }
 
@@ -119,15 +132,14 @@ async function sdbgfbonoffPr() {
     }
 }
 
-//smtbgdldvcPr("open");  //확인용 스위치도 만들기!
 async function sendemailPr(sendemjson) {
     const authClient = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
     authClient.setCredentials({ refresh_token: REFRESH_TOKEN });
-  
+
     var to = sendemjson.to;
     var subject = sendemjson.subject;
     var message = sendemjson.message;
-  
+
     const accessToken = await authClient.getAccessToken();
     const transport = nodemailer.createTransport({
         service: 'gmail',
@@ -149,6 +161,35 @@ async function sendemailPr(sendemjson) {
     };
     const result = await transport.sendMail(mailOptions);
     //console.log(result);
-  }
+}
 
-module.exports = { smtbgdldvcPr,sendemailPr };
+
+async function chkpasscd(timechk, pcchk) {
+    // //스프레드시트 ID, 시트 이름, 가져올 범위를 설정합니다.
+    const sheetName = process.env.DCSTNM;
+    const range = 'A2:L';
+
+    const authClient = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+    authClient.setCredentials({ refresh_token: REFRESH_TOKEN2 });
+    const sheets = google.sheets({ version: 'v4', auth: authClient });
+    const response = await sheets.spreadsheets.values.get({  //sheet get!
+        spreadsheetId: SHEET_ID,
+        range: `${sheetName}!${range}`,
+    });
+    const values = response.data.values;
+    let chkrst = false;
+    let chkrstnm = "";
+    for (var i = 0; i < values.length; i++) {
+        if (values[i][2] == pcchk) {
+            chkrst = true;
+            chkrstnm = values[i][1];
+            //console.log("일치항목 체크확인!:"+values[0][2]);
+
+        }
+    }
+    return { chkrst: chkrst, chkrstnm: chkrstnm };
+}
+
+//smtbgdldvcPr("open");
+//chkpasscd();
+module.exports = { smtbgdldvcPr, sendemailPr, chkpasscd };
